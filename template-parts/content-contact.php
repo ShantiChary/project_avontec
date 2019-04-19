fa896387a50fa4a29f45edb63157e1f7aa88727d
Shanti Chary <shantichary@gmail.com>
2019-04-18 23:02:20 -0700

    Main and secondary menu styling

--- template-parts/content-contact.php
+++ template-parts/content-contact.php
@@ -12,14 +12,49 @@
 
             <div class="internal-section-1">
 
-                <div class="about-section-1-text">
-                    <?php
-                        if(function_exists('get_field')) {
-                            // echo '<p>';
-                            // the_field('about_us_text');
-                            // echo '</p>';
-                        }
-                    ?>
+
+            <div class="about-section-main">
+                    <!-- About Us banner image -->
+
+                    <?php 
+                        $image = get_field('about_us_banner', 'option');
+
+                        if( !empty($image) ): ?>
+                            <div class="banner-sub-menu">
+                                <div class="banner-img-div">
+                                    <!-- <img class="banner-img" src="?php echo $image['url']; ?>" alt="?php echo $image['alt']; ?>" /> -->
+                                </div>
+
+                                <h1 class="banner-h1"><?php echo get_the_title(); ?> </h1>
+
+                                <!-- Sub menu div -->
+                                <div class="sub-menu-div">
+
+                                <h3><?php echo 'More' . ' ' . get_the_title( $post->post_parent ); ?></h3>
+                                    <?php echo do_shortcode('[wpb_childpages]'); ?>
+                                </div>
+                            </div>
+
+                    <?php endif; ?>
+
+                    <div class="about-section-text">
+                        <?php
+                                // loop through the rows of data
+                                while ( have_rows('about_us_section') ) : the_row();
+                                    // display a sub field value
+                                    //variable
+                                    $aboutUsHeading = get_sub_field('about_us_heading');
+                                    $aboutUsText = get_sub_field('about_us_text');
+
+                                    ?>
+
+                                    <h2><?php echo $aboutUsHeading; ?></h2>
+                                    <p><?php echo $aboutUsText; ?></p>
+                
+                                <?php
+                                endwhile;
+                            ?>
+                    </div>
                 </div>
 
                 <?php get_sidebar(); ?>
