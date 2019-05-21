4156779b6c74b8e8654c88db4e446ae8d7e0deb2
Shanti Chary <shantichary@gmail.com>
2019-05-21 16:33:26 -0700

    Sub menu and set up new pages

---
+++ template-parts/content-overhead-conveyors.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Template part for displaying page content in elevating-solutions.php
+ *
+ * @package avontec
+ */
+
+?>
+
+<!-- Elevating Solutions Page Content -->
+
+<div class="about-section">
+        <?php
+            $ppText = get_field('pp_text');
+            $ppImage = get_field('pp_image'); ?>
+
+            <div class="conveying-list-text">
+                <ul>
+                    <?php echo $ppText; ?>
+                </ul>
+            </div>
+
+            <?php   
+                if( !empty($ppImage) ): ?>
+                    <img src="<?php echo $ppImage['url']; ?>" alt="<?php echo $ppImage['alt']; ?>" width=100%/>
+            <?php endif;  
+        ?>
+</div>
+
+
+
